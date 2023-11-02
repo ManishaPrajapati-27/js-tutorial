2aaeb56f201f34764666f8cecbd768f0676c0c5e
Manisha Prajapati <104721952+ManishaPrajapati-27@users.noreply.github.com>
2023-11-02 13:37:03 +0000

    objects properties

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -5,18 +5,57 @@ const array = [1, 4, 5, 7, 8, 9];
 // array.unshift(2)
 // array.shift()
 
-
 // console.log(array.includes(3))
 // console.log(array.indexOf(7))
 
 // console.log(array.join());
 
-console.log(array)
+// console.log(array)
 
-let newArr = array.slice(2, 6)
-console.log(newArr)
+let newArr = array.slice(2, 6);
+// console.log(newArr)
 
 // Splice change the original array
-let newArr2 = array.splice(2, 6)
-console.log(array);
-console.log(newArr2)
+let newArr2 = array.splice(2, 6);
+// console.log(array);
+// console.log(newArr2)
+
+// // // // // // More About Array // // // // // //
+
+const heros = ["Superman", "Shaktiman", "Batman", "Ironman", "Thor"];
+const slayers = ["Tanjirou", "Ineskou", "Zenitsou", "Tomiaka", "Orakodaki"];
+
+// heros.push(slayers)
+// console.log(heros)
+
+// Create New array concatinate array
+// let newHeros = heros.concat(slayers)
+// console.log(newHeros)
+
+// Spread all array and create new array
+let newHeros = [...heros, ...slayers];
+
+console.log(newHeros);
+
+let array2 = [1, 2, 3, 4, 5, [6, 7, 8], [4, 6, 4, 2, 5, [6, 7]]];
+
+// Flat all array and create new simple array.
+let anotherArray = array2.flat(3)
+console.log(anotherArray);
+
+// Check it's array or not
+console.log(Array.isArray("Heyy..."))
+
+// Convert in array
+console.log(Array.from("Heyy..."))
+
+// Show empty array it's not converted
+console.log(Array.from({welcome: "Heyy..."}))
+
+let marks1 = 100
+let marks2 = 200
+let marks3 = 300
+let marks4 = 400
+
+// Create in array of variables
+console.log(Array.of(marks1, marks2, marks3, marks4))
---
+++ 02_basics/objects.js
@@ -0,0 +1,112 @@
+// Single ton
+// In we use object.create to create object
+
+// Object Literals
+// Simple Object
+
+// Symbol in object
+const myKey = Symbol("key1");
+
+const object = {
+  name: "Manisha",
+  marks: 100,
+  [myKey]: "4839GHJN79",
+  age: 21,
+  email: "manisha@google.com",
+  isLoggedIn: true,
+};
+
+// console.log(object.marks)
+// console.log(object["marks"])
+// console.log(object["email"])
+// console.log(object[myKey])
+// console.log(object)
+
+// For chage value
+object.marks = 80;
+// console.log(object)
+
+//
+
+object.myFuction = function () {
+  console.log(`Hii.. hello.. Namaste`);
+};
+
+object.myFuction2 = function () {
+  console.log(`Hii.. hello.. Namaste ${this.name}`);
+};
+
+// console.log(object.myFuction())
+// console.log(object.myFuction2())
+
+// /////////////////// More About Objects ////////////////////// //
+
+// Single ton
+// const tinderUser = new Object()
+
+// Non singleton
+const tinderUser = {};
+// console.log(tinderUser)
+
+tinderUser.name = "Rahul";
+tinderUser.age = 23;
+tinderUser.isLoggedIn = true;
+tinderUser.date = 3;
+
+// console.log(tinderUser)
+
+const anotherTinderUser = {
+  fullname: {
+    name: {
+      firstName: "Manisha",
+      lastName: "Prajapati",
+    },
+  },
+  age: 21,
+  inLoggedIn: false,
+};
+
+// console.log(anotherTinderUser);
+// console.log(anotherTinderUser.fullname.name.firstName);
+
+let obj1 = { a: "2", b: "3" };
+let obj2 = { c: "4", d: "5" };
+let obj3 = { f: "6", e: "7" };
+
+// Concatinate Objects
+// let obj4 = Object.assign({}, obj1, obj2, obj3)
+// For spread
+let obj4 = {...obj1, ...obj2, ...obj3}
+console.log(obj4)
+
+
+const numbers =[
+    {
+        num1: 2,
+        num2: 2
+    },
+    {
+        num1: 2,
+        num2: 2
+    },
+    {
+        num1: 2,
+        num2: 2
+    },
+]
+
+// console.log(numbers[1].num1)
+
+console.log(tinderUser)
+// For All key
+console.log(Object.keys(tinderUser))
+// For all values
+console.log(Object.values(tinderUser))
+
+// All enteries in array form
+console.log(Object.entries(tinderUser))
+
+// For check key is there or not
+console.log(Object.hasOwnProperty("name"))
+
+
