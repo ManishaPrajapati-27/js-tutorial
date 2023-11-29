2c50817ca0124fc3c59eea52acf41bf13f676e07
Manisha Prajapati <104721952+ManishaPrajapati-27@users.noreply.github.com>
2023-11-29 10:48:07 +0000

    basic projects

--- 06_basic_projects/js/app.js
+++ 06_basic_projects/js/app.js
@@ -41,3 +41,146 @@ buttons.forEach((button) => {
     }
   });
 });
+
+
+// BMI Calculator
+
+const form = document.querySelector(".form")
+const formBmi = () =>{
+  form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const height = parseInt(document.getElementById("height").value);
+    const weight = parseInt(document.getElementById("weight").value);
+    const result = document.getElementById("result");
+    const resultRange = document.getElementById("resultRange");
+  
+    if(height === " " || height < 0 || isNaN(height)){
+      result.innerHTML = `<h4> Please add the valid number ${height} </h4>`
+    }else if(weight === " " || weight < 0 || isNaN(weight)){
+      result.innerHTML = `<h4> Please add the valid number ${height} </h4>`
+    }else{
+      const bmi = (weight / ((height * height)/10000)).toFixed(2)
+      result.innerHTML = `<h4> BMI : ${bmi} </h4>`
+      if (bmi < 18.5) { 
+        resultRange.innerHTML = `<h4> You are in the underweight range </h4>`;
+      } else if (bmi >= 18.5 && bmi <= 25.0) {
+          resultRange.innerHTML = `<h4> You are in the healthy weight range </h4>`;
+      } else if (bmi > 25.0 && bmi <= 29.9) {
+          resultRange.innerHTML = `<h4> You are in the overweight range </h4>`;
+      } else {
+          resultRange.innerHTML = `<h4> You are in the obese range </h4>`;
+      }
+    }
+  })
+}
+
+
+// Digital Clock
+
+const digiClock = () =>{
+  const clock = document.querySelector(".digital-clock .clock");
+  
+  // let date = new Date()
+  // console.log(date.toLocaleTimeString());
+  
+  setInterval(()=>{
+    let date = new Date()
+    clock.innerHTML = `<h4>${date.toLocaleTimeString()}</h4>`
+  },1000)
+}
+
+// Guess Number
+
+let randomNum = parseInt(Math.random() * 100 + 1);
+
+const submit = document.getElementById("subBtn");
+const userInput = document.querySelector(".input-num");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".attemts");
+const lowOrHigh = document.querySelector(".low-or-high");
+const startOver = document.querySelector(".info");
+const alertMsg = document.getElementById("alert-ms");
+
+const button = document.createElement("button");
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+  submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const guess = parseInt(userInput.value)
+    console.log(guess);
+    validateGuess(guess);
+  })
+}
+
+validateGuess = (guess) =>{
+  if(isNaN(guess)){
+    alertMsg.innerHTML = "Please enter a valide number";
+  }else if(guess < 1 ){
+    alertMsg.innerHTML = "Please enter a number greater then 1";
+  }else if(guess > 100 ){
+    alertMsg.innerHTML = "Please enter a number less then 100";
+  }else{
+    prevGuess.push(guess);
+    if(numGuess === 11){
+      displayGuess();
+      displayMessage(`Game over Random number was ${randomNum}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+
+checkGuess = (guess) =>{
+  if(guess === randomNum){
+    displayMessage(`You guess the right number well done.`)
+    endGame();
+  }else if(guess < randomNum){
+    displayMessage(`Number is Tooo Low`)
+  }else if(guess > randomNum){
+    displayMessage(`Number is Tooo High`)
+  }
+}
+
+displayGuess = (guess) =>{
+  userInput.value = "";
+  guessSlot.innerHTML += `${guess}, `
+  numGuess++;
+  remaining.innerHTML = `${11 - numGuess}`
+}
+
+displayMessage = (message) =>{
+  lowOrHigh.innerHTML = `${message}`
+}
+
+endGame = () =>{
+  userInput.value = "";
+  userInput.setAttribute("disabled", "")
+  button.classList.add("button");
+  button.innerHTML=`<span id="newGame">Start new Game</span>`;
+  startOver.appendChild(button);
+  playGame = false;
+  newGame();
+}
+
+newGame = () =>{
+const newGameButton = document.querySelector("#newGame");
+newGameButton.addEventListener("click", ()=>{
+  randomNum = parseInt(Math.random() * 100 + 1);
+  prevGuess = [];
+  numGuess = 1;
+  guessSlot.innerHTML = "";
+  remaining.innerHTML = `${11 - numGuess}`
+  userInput.removeAttribute("disabled");
+  startOver.removeChild(button);
+  playGame = true;
+})
+}
+
+
