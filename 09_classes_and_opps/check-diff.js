1ec3d1fcb7d929ade61e14af2e7d406e54e76d12
Manisha Prajapati <104721952+ManishaPrajapati-27@users.noreply.github.com>
2023-12-27 13:36:02 +0000

    lexical scope & closure

---
+++ 09_classes_and_opps/check-diff.js
@@ -0,0 +1,16 @@
+const str = "Manisha";
+const str1 = new String("Manisha");
+
+console.log(str === str1)
+console.log( typeof(str1))
+
+function myFunction(a, a){
+return a + a;
+}
+
+console.log(myFunction(5, 5));
+
+// const myFunc = (a, a) =>{
+//     return a + a;
+// }
+// console.log(myFunc(5, 5));
